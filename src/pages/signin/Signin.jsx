c9589fb0e71de9216f9a5c40ff06f3c187d843de
MisterJohney <johneylikestoplay@gmail.com>
2025-02-17 10:08:50 +0200

    Compleated login page UI

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -8,10 +8,10 @@ const Signin = () => {
       <div className="signin-box">
         <h1>Sign in to TurboPlop</h1>
         <label for="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" className="signin-input" id="username" name="username" />
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" />
-        <input type="submit" value="Sign in" />
+        <input type="password" className="signin-input" id="password" name="password" />
+        <input type="submit" className="signinbutton" value="Sign in" />
       </div>
     </div>
   );
