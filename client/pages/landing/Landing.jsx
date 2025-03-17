5b49d8c9aba172e3cf3761560baf470ff8597c74
MisterJohney <joneywantstoplay@gmail.com>
2025-03-17 18:16:41 +0200

    added basic file upload

--- client/pages/landing/Landing.jsx
+++ client/pages/landing/Landing.jsx
@@ -6,7 +6,12 @@ const Landing = () => {
   return (
     <div className="landing">
       <div className="landing-left">
-        <a href="/" className="uploadButton">Upload</a>
+
+        <form action="http://localhost:5000/api/upload" method="post" encType="multipart/form-data">
+            <input type="file" name="upload-files" multiple />
+            <input type="submit" className="uploadButton" value="Upload" />
+        </form>
+        
       </div>
       <div className="landing-right">
         <h1>Blazingly fast way to upload your files</h1>
--- server/index.js
+++ server/index.js
@@ -1,17 +1,47 @@
+"use strict"
+
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
-//const logger = require("morgan");
-const cors = require("cors");
+//const cookieParser = require("cookie-parser");
+const multer = require("multer")
 
 const app = express();
-//app.use("cors");
-//app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({ storage: storage });
+
+
+app.use("/uploads", express.static("uploads"));
+
 
 app.get('/api', (req, res) => {
-  res.json({"users": ["one", "two", "three"]})
-})
+  res.json({"users": ["one", "two", "three"]});
+});
+
+app.post('/api/upload', upload.array("upload-files", 10), (req, res) => {
+  if (!req.files || req.files.length === 0) {
+      return res.status(400).send("No file");
+  }
+  let downloadLinks = req.files.map(file => {
+      // TODO: make so when downloading it downloads with uploaded name
+    return `<a href="/uploads/${file.filename}" download>Download ${file.originalname}</a>`;
+}).join('<br>');
+    res.send(`
+    <h2>Files uploaded successfully!</h2>
+    <div>${downloadLinks}</div>
+    <br>
+  `);
+});
+
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
-})
+});
