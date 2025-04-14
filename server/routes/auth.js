abafbd6a8712f1c04581ad25a9b8ef2bf357f8ed
MisterJohney <johneylikestoplay@gmail.com>
2025-04-14 12:56:19 +0300

    unfinished auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const config = require("../config.js");
 
 const sqlhelper = require("../utils/sqlhelper.js");
 const sql = new sqlhelper();
+
 
 router.post('/signin', async (req, res) => {
   const sql = new sqlhelper();
   try {
     await sql.connect('turboplop.db');
     const isLoginValid = await sql.isValidLogin(req.body);
-    res.json(isLoginValid);
+    
+    if (isLoginValid) {
+      const token = jwt.sign({req.body.username, req.body.password}, config.config.secretKey);
+      res.cookie("authcookie", token, {maxAge:900000,httpOnly:true})
+    } else {
+      res.json(isLoginValid);
+    }
   } catch (error) {
     console.error(error);
   } finally {
