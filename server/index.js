2ed8d06ca2ec9fa70df96fd9a2a3bb330ddfda47
MisterJohney <joneywantstoplay@gmail.com>
2025-04-15 11:48:22 +0300

    Started on jwt

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use("/uploads", express.static("uploads"));
 
 // Routes
 
-app.use("/api", authRoutes);
+app.use(authRoutes);
 app.use("/api", uploadRoute);
 app.use("/api", homeRoute);
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,20 +2,24 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const config = require("../config.js");
+const bodyParser = require("body-parser");
 
 const sqlhelper = require("../utils/sqlhelper.js");
 const sql = new sqlhelper();
 
 
-router.post('/signin', async (req, res) => {
+router.post('/api/signin', async (req, res) => {
   const sql = new sqlhelper();
   try {
     await sql.connect('turboplop.db');
     const isLoginValid = await sql.isValidLogin(req.body);
     
     if (isLoginValid) {
-      const token = jwt.sign({req.body.username, req.body.password}, config.config.secretKey);
-      res.cookie("authcookie", token, {maxAge:900000,httpOnly:true})
+      const { username, password } = req.body;
+      const token = jwt.sign({username, password}, config.config.secretKey);
+      res.cookie("authcookie", token, {maxAge:900000,httpOnly:true});
+      // res.json(isLoginValid);
+      res.redirect("http://localhost:5173/home");
     } else {
       res.json(isLoginValid);
     }
